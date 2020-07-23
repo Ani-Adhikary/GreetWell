97205ad507f5e48cb3cb41ab7e6754c0fecb7e73
Ani Adhikary <aniadhikary@Anis-MacBook-Air.local>
2020-07-23 18:09:17 +0530

    Added WDay Greetings

--- GreetWell/Utils/GreetingConstants/WomansDayGreetingConstants.swift
+++
@@ -1,9 +0,0 @@
-//
-//  WomansDayGreetingConstants.swift
-//  GreetWell
-//
-//  Created by Ani Adhikary on 06/07/20.
-//  Copyright © 2020 Ani Adhikary. All rights reserved.
-//
-
-import Foundation
---
+++ GreetWell/Utils/GreetingConstants/WomensDayGreetingConstants.swift
@@ -0,0 +1,69 @@
+//
+//  WomensDayGreetingConstants.swift
+//  GreetWell
+//
+//  Created by Ani Adhikary on 23/07/20.
+//  Copyright © 2020 Ani Adhikary. All rights reserved.
+//
+
+import Foundation
+
+struct WomensDayGreetingConstants {
+    
+    // MARK: - Greeting 1
+    
+    static let wdayGreeting1ID = 1
+    static let wdayGreetingText = "Happy Women's Day"
+    static let wdayGreeting1Image = "WomensDay1"
+    
+    // MARK: - Greeting 2
+    
+    static let wdayGreeting2ID = 2
+    static let wdayGreeting2Image = "WomensDay2"
+    
+    // MARK: - Greeting 3
+    
+    static let wdayGreeting3ID = 3
+    static let wdayGreeting3Image = "WomensDay3"
+    
+    // MARK: - Greeting 4
+    
+    static let wdayGreeting4ID = 4
+    static let wdayGreeting4Image = "WomensDay4"
+    
+    // MARK: - Greeting 5
+    
+    static let wdayGreeting5ID = 5
+    static let wdayGreeting5Image = "WomensDay5"
+    
+    // MARK: - Greeting 6
+    
+    static let wdayGreeting6ID = 6
+    static let wdayGreeting6Image = "WomensDay6"
+    
+    // MARK: - Greeting 7
+    
+    static let wdayGreeting7ID = 7
+    static let wdayGreeting7Image = "WomensDay7"
+    
+    // MARK: - Greeting 8
+    
+    static let wdayGreeting8ID = 8
+    static let wdayGreeting8Image = "WomensDay8"
+    
+    // MARK: - Greeting 9
+    
+    static let wdayGreeting9ID = 9
+    static let wdayGreeting9Image = "WomensDay9"
+    
+    // MARK: - Greeting 10
+    
+    static let wdayGreeting10ID = 10
+    static let wdayGreeting10Image = "WomensDay10"
+    
+    // MARK: - Greeting 11
+    
+    static let wdayGreeting11ID = 11
+    static let wdayGreeting11Image = "WomensDay11"
+    
+}
