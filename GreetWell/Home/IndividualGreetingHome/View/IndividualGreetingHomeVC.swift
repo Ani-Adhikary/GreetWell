34aeea609dce61e79d0b7f91a53987067b239b31
Ani Adhikary <aniadhikary@Anis-MacBook-Air.local>
2020-08-16 01:44:01 +0530

    Select Bar Button added

--- GreetWell/Home/IndividualGreetingHome/View/IndividualGreetingHomeVC.swift
+++ GreetWell/Home/IndividualGreetingHome/View/IndividualGreetingHomeVC.swift
@@ -28,13 +28,13 @@ class IndividualGreetingHomeVC: UIViewController {
         didSet {
             switch mMode {
             case .view:
-               for (key, value) in dictionarySelectedIndecPath {
-                 if value {
-                   collectionView.deselectItem(at: key, animated: true)
-                 }
-               }
-               
-               dictionarySelectedIndecPath.removeAll()
+                for (key, value) in dictionarySelectedIndecPath {
+                    if value {
+                        collectionView.deselectItem(at: key, animated: true)
+                    }
+                }
+                
+                dictionarySelectedIndecPath.removeAll()
                 
                 selectBarButton.title = "Select"
                 navigationItem.leftBarButtonItem = nil
@@ -44,8 +44,8 @@ class IndividualGreetingHomeVC: UIViewController {
             case .select:
                 selectBarButton.title = "Cancel"
                 collectionView.allowsMultipleSelection = true
-                shareBarButtonItem.isEnabled = true
-                favBarButtonItem.isEnabled = true
+                //                shareBarButtonItem.isEnabled = true
+                //                favBarButtonItem.isEnabled = true
             }
         }
     }
@@ -102,9 +102,9 @@ class IndividualGreetingHomeVC: UIViewController {
         
         var shareSelectedIndexPaths: [IndexPath] = []
         for (key, value) in dictionarySelectedIndecPath {
-          if value {
-            shareSelectedIndexPaths.append(key)
-          }
+            if value {
+                shareSelectedIndexPaths.append(key)
+            }
         }
         
         var images = [UIImage]()
@@ -119,7 +119,7 @@ class IndividualGreetingHomeVC: UIViewController {
         }
         
         print("images count---------- \(images.count)")
-
+        
         let activityViewController = UIActivityViewController(activityItems: images, applicationActivities: nil)
         
         if DeviceEnv.isIpad {
@@ -127,7 +127,18 @@ class IndividualGreetingHomeVC: UIViewController {
             activityViewController.popoverPresentationController?.sourceRect = CGRect(x: self.view.bounds.midX, y: self.view.bounds.midY, width: 0, height: 0)
         }
         self.present(activityViewController, animated: true, completion: nil)
-        mMode = .view
+        
+//        activityViewController.completionWithItemsHandler = { activity, success, items, error in
+//            print("activity: \(activity), success: \(success), items: \(items), error: \(error)")
+//
+//            self.mMode = .view
+//        }
+        
+        activityViewController.completionWithItemsHandler = { activity, success, items, error in
+            self.mMode = .view
+        }
+        
+        
     }
     
     @IBAction func favBarButtonClicked(_ sender: UIBarButtonItem) {
@@ -153,30 +164,44 @@ extension IndividualGreetingHomeVC: UICollectionViewDataSource, UICollectionView
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-//        let detailVC = DetailViewController(nibName: "DetailViewController", bundle: nil)
-//        detailVC.greetings = greetings
-//        detailVC.greetingFromHome = greetings[indexPath.row]
-//        navigationController?.pushViewController(detailVC, animated: true)
-//        collectionView.deselectItem(at: indexPath, animated: true)
+        //        let detailVC = DetailViewController(nibName: "DetailViewController", bundle: nil)
+        //        detailVC.greetings = greetings
+        //        detailVC.greetingFromHome = greetings[indexPath.row]
+        //        navigationController?.pushViewController(detailVC, animated: true)
+        //        collectionView.deselectItem(at: indexPath, animated: true)
         
         switch mMode {
         case .view:
-          collectionView.deselectItem(at: indexPath, animated: true)
-          let detailVC = DetailViewController(nibName: "DetailViewController", bundle: nil)
-          detailVC.greetings = greetings
-          detailVC.greetingFromHome = greetings[indexPath.row]
-          navigationController?.pushViewController(detailVC, animated: true)
-          collectionView.deselectItem(at: indexPath, animated: true)
+            collectionView.deselectItem(at: indexPath, animated: true)
+            let detailVC = DetailViewController(nibName: "DetailViewController", bundle: nil)
+            detailVC.greetings = greetings
+            detailVC.greetingFromHome = greetings[indexPath.row]
+            navigationController?.pushViewController(detailVC, animated: true)
+            collectionView.deselectItem(at: indexPath, animated: true)
         case .select:
-          dictionarySelectedIndecPath[indexPath] = true
-          //print("Item selected")
+            dictionarySelectedIndecPath[indexPath] = true
+            shareBarButtonItem.isEnabled = true
+            favBarButtonItem.isEnabled = true
+            //print("Item selected")
         }
     }
     
     func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
-      if mMode == .select {
-        dictionarySelectedIndecPath[indexPath] = false
-      }
+        if mMode == .select {
+            dictionarySelectedIndecPath[indexPath] = false
+        }
+//        if dictionarySelectedIndecPath.count == 0 {
+//            shareBarButtonItem.isEnabled = false
+//            favBarButtonItem.isEnabled = false
+//        }
+        
+//        for (_, value) in dictionarySelectedIndecPath {
+//            if !value {
+//                //collectionView.deselectItem(at: key, animated: true)
+//                shareBarButtonItem.isEnabled = false
+//                favBarButtonItem.isEnabled = false
+//            }
+//        }
     }
 }
 
