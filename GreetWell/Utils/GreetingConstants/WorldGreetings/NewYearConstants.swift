b45b113fc397903c4488533c69dea34764d8dbf2
Ani <aniadhikary@Anis-MacBook-Air.local>
2021-07-10 21:59:05 +0530

    New year greetings added

---
+++ GreetWell/Utils/GreetingConstants/WorldGreetings/NewYearConstants.swift
@@ -0,0 +1,65 @@
+//
+//  NewYearConstants.swift
+//  GreetWell
+//
+//  Created by Ani Adhikary on 10/07/21.
+//  Copyright © 2021 Ani Adhikary. All rights reserved.
+//
+
+import Foundation
+
+struct NewYearConstants {
+    
+    // MARK: - Greeting 1
+    
+    static let newYearGreeting1ID = 1
+    static let newYearGreetingText = "Happy New Year"
+    static let newYearAccLabel = "Happy New Year"
+    static let newYearGreeting1Image = "NewYear1"
+    
+    // MARK: - Greeting 2
+    
+    static let newYearGreeting2ID = 2
+    static let newYearGreeting2Image = "NewYear3"
+    
+    // MARK: - Greeting 3
+    
+    static let newYearGreeting3ID = 3
+    static let newYearGreeting3Image = "NewYear3"
+    
+    // MARK: - Greeting 4
+    
+    static let newYearGreeting4ID = 4
+    static let newYearGreeting4Image = "NewYear4"
+    
+    // MARK: - Greeting 5
+    
+    static let newYearGreeting5ID = 5
+    static let newYearGreeting5Image = "NewYear5"
+    
+    // MARK: - Greeting 6
+    
+    static let newYearGreeting6ID = 6
+    static let newYearGreeting6Image = "NewYear6"
+    
+    // MARK: - Greeting 7
+    
+    static let newYearGreeting7ID = 7
+    static let newYearGreeting7Image = "NewYear7"
+    
+    // MARK: - Greeting 8
+    
+    static let newYearGreeting8ID = 8
+    static let newYearGreeting8Image = "NewYear8"
+    
+    // MARK: - Greeting 9
+    
+    static let newYearGreeting9ID = 9
+    static let newYearGreeting9Image = "NewYear9"
+    
+    // MARK: - Greeting 10
+    
+    static let newYearGreeting10ID = 10
+    static let newYearGreeting10Image = "NewYear10"
+    
+}
