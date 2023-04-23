81d73f88c5c21fad86264b53271830dd6d83060b
Ani Adhikary <aniadhikary@Anis-MacBook-Air.local>
2023-04-23 16:40:23 +0530

    Year updated

--- GreetWell/Quotes/QuoteList/Entity/GreetingListResponse.swift
+++ GreetWell/Quotes/QuoteList/Entity/GreetingListResponse.swift
@@ -3,7 +3,7 @@
 //  GreetWell
 //
 //  Created by Ani Adhikary on 17/07/20.
-//  Copyright © 2020 Ani Adhikary. All rights reserved.
+//  Copyright © 2023 Ani Adhikary. All rights reserved.
 //
 
 import Foundation
